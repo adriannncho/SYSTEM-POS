73d2ea2b77813ba06e045879693b062b9f521927
CristianLaverde <laverdemc77@gmail.com>
2025-02-27 21:27:34 -0500

    ADD-TASK8 - Se agrega componente menu collapsed

---
+++ frontend/System-Post/src/app/core/layout/components/collapsed-menu/collapsed-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collapsed-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './collapsed-menu.component.html',
+  styleUrl: './collapsed-menu.component.css'
+})
+export class CollapsedMenuComponent {
+
+}
