dbcf33ce2611b418bd0de5c174fb82b061c17858
CristianLaverde <laverdemc77@gmail.com>
2025-03-04 22:27:25 -0500

    UPDATE-TASK-8-Se ajustan menus de la app

--- frontend/System-Post/src/app/core/layout/components/collapsed-menu/collapsed-menu.component.ts
+++ frontend/System-Post/src/app/core/layout/components/collapsed-menu/collapsed-menu.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NG_ZORRO_MODULES } from '../../../../shared/config/ng-zorro.config';
 
 @Component({
   selector: 'app-collapsed-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    ...NG_ZORRO_MODULES
+  ],
   templateUrl: './collapsed-menu.component.html',
   styleUrl: './collapsed-menu.component.css'
 })
 export class CollapsedMenuComponent {
+
+  @Input() isCollapsed: boolean = false;
 
 }
