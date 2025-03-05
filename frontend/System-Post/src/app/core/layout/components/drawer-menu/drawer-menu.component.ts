e7b4617cd6095505664d0e8b0ee0987563c385e7
CristianLaverde <laverdemc77@gmail.com>
2025-03-04 22:26:48 -0500

    UPDATE-TASK-8-Se ajusta menu responsive

--- frontend/System-Post/src/app/core/layout/components/drawer-menu/drawer-menu.component.ts
+++ frontend/System-Post/src/app/core/layout/components/drawer-menu/drawer-menu.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NG_ZORRO_MODULES } from '../../../../shared/config/ng-zorro.config';
 
 @Component({
   selector: 'app-drawer-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    ...NG_ZORRO_MODULES
+  ],
   templateUrl: './drawer-menu.component.html',
   styleUrl: './drawer-menu.component.css'
 })
 export class DrawerMenuComponent {
+
+  @Input() isCollapsed: boolean = false;
+  @Output() hideMenuEmitter = new EventEmitter<boolean>();
+
+  closeMenu(): void {
+    this.hideMenuEmitter.emit(false);
+  }
 
 }
