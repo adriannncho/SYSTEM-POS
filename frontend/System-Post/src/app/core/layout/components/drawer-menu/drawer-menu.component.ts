b557dded2529f20d5740e01b173c898ec8c1269c
CristianLaverde <laverdemc77@gmail.com>
2025-03-04 22:00:10 -0500

    ADD-TASK-8-Se agrega componente para menu en responsive >768px

---
+++ frontend/System-Post/src/app/core/layout/components/drawer-menu/drawer-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drawer-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './drawer-menu.component.html',
+  styleUrl: './drawer-menu.component.css'
+})
+export class DrawerMenuComponent {
+
+}
