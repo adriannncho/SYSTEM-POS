fb78d1dae9472818ea5f17ffb6abb3d8321f04f5
adrianherrera23 <adrian2005herrera@gmail.com>
2025-03-10 19:30:16 -0500

    ADD-SP Se agrega servicio para obtener los pedidos realizados

---
+++ backend/sp-processor/src/main/java/org/sp/processor/repository/OrderRepository.java
@@ -0,0 +1,16 @@
+package org.sp.processor.repository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.sp.processor.domain.order.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+@ApplicationScoped
+public interface OrderRepository extends JpaRepository<Orders, Long> {
+
+    @Query("SELECT o FROM Orders o LEFT JOIN FETCH o.detailsOrders")
+    List<Orders> findAllWithDetails();
+
+}
---
+++ backend/sp-processor/src/main/java/org/sp/processor/rest/OrderApi.java
@@ -0,0 +1,81 @@
+package org.sp.processor.rest;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.media.SchemaProperty;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.sp.processor.helper.exception.HandlerException;
+import org.sp.processor.helper.exception.ProblemException;
+import org.sp.processor.service.OrderService;
+
+@Path("/sp-processor")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class OrderApi {
+
+    @Inject
+    OrderService orderService;
+
+    @GET
+    @Path("/orders")
+    @APIResponses(
+            value = {
+                    @APIResponse(
+                            responseCode = "200",
+                            description = "Se obtiene el listado de pedidos correctamente",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON,
+                                    schema = @Schema(
+                                            type = SchemaType.ARRAY,
+                                            example = """
+                                                    [
+                                                   
+                                                    ]"""
+                                    )
+                            )
+                    ),
+                    @APIResponse(
+                            responseCode = "404",
+                            description = "No hay registros de pedidos en base de datos.",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON,
+                                    schema = @Schema(
+                                            implementation = ProblemException.class,
+                                            properties = {
+                                                    @SchemaProperty(
+                                                            name = "detail",
+                                                            example = "No se encontraron registro de pedidos."
+                                                    )
+                                            }
+                                    )
+                            )
+                    ),
+                    @APIResponse(
+                            responseCode = "500",
+                            description = "Error interno de servidor",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON,
+                                    schema = @Schema(implementation = HandlerException.ResponseError.class)
+                            )
+                    )
+            }
+    )
+    @Operation(
+            summary = "Obtener listado de pedidos",
+            description = "Se obtiene el listado con la información de los pedidos registrados"
+    )
+    public Response getOrders() {
+        return Response.ok().entity(orderService.getOrders()).build();
+    }
+
+}
