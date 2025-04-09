eb9395b107097f8fb0de63373be8a9cb4e7710a4
adrianherrera23 <adrian2005herrera@gmail.com>
2025-04-08 21:54:47 -0500

    ADD-SP Se agrega versionamiento numero 1 de la base de datos

---
+++ versionamientoDB/version1/pos_system_detalle_pedido.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: pos_system
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `detalle_pedido`
+--
+
+DROP TABLE IF EXISTS `detalle_pedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detalle_pedido` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `PEDIDO_ID` int NOT NULL,
+  `PRODUCTO_ID` int NOT NULL,
+  `CANTIDAD` int NOT NULL,
+  `OBSERVACION` varchar(255) NOT NULL,
+  `PRECIO_UNITARIO` decimal(10,2) NOT NULL,
+  `SUBTOTAL` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `PEDIDO_ID` (`PEDIDO_ID`),
+  KEY `PRODUCTO_ID` (`PRODUCTO_ID`),
+  CONSTRAINT `detalle_pedido_ibfk_1` FOREIGN KEY (`PEDIDO_ID`) REFERENCES `pedidos` (`ID`) ON DELETE CASCADE,
+  CONSTRAINT `detalle_pedido_ibfk_2` FOREIGN KEY (`PRODUCTO_ID`) REFERENCES `productos` (`ID`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detalle_pedido`
+--
+
+LOCK TABLES `detalle_pedido` WRITE;
+/*!40000 ALTER TABLE `detalle_pedido` DISABLE KEYS */;
+INSERT INTO `detalle_pedido` VALUES (26,18,1,2,'Sin cebolla',10000.00,20000.00),(27,18,2,1,'Extra queso',15000.00,15000.00),(28,19,2,3,'Picante',8500.00,17000.00),(29,20,1,4,'Sin sal',30000.00,120000.00),(30,21,1,4,'Sin sal',15.00,60.00),(31,22,1,4,'Sin sal',35000.00,140000.00),(32,23,6,2,'Las salsas aparte',50000.00,100000.00),(33,24,6,2,'Las salsas aparte',50000.00,100000.00),(35,25,1,2,'',35000.00,70000.00);
+/*!40000 ALTER TABLE `detalle_pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-08 21:52:27
